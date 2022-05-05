4337b52cbcb46aec4064bbf7e5af223f4162df0a
18435111782 <1780934510@qq.com>
2022-05-05 10:36:59 +0800

    application

--- application.all/src/main/java/fm/douban/PracticeApplication.java
+++ application.all/src/main/java/fm/douban/PracticeApplication.java
@@ -1,11 +1,9 @@
 package fm.douban;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"fm.douban"})
-@MapperScan(basePackages = {"fm.douban.dao"})
+@SpringBootApplication
 class PracticeApplication {
 
     public static void main(String[] args) {
